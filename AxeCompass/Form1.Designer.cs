9464c6ed6511f7b6d239e4ff5c27fd180163cf13
NikolayFedyaev <fedyaevnv@mail.ru>
2022-10-21 13:42:54 +0700

    Проект Системы и интерфейс

---
+++ AxeCompass/Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿
+namespace AxeCompass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.AxeLengthTextBox = new System.Windows.Forms.TextBox();
+            this.AxeLengthLabel = new System.Windows.Forms.Label();
+            this.ParametersLabel = new System.Windows.Forms.Label();
+            this.MaximumButton = new System.Windows.Forms.Button();
+            this.AverageButton = new System.Windows.Forms.Button();
+            this.MinimumButton = new System.Windows.Forms.Button();
+            this.BuildButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label14);
+            this.splitContainer1.Panel1.Controls.Add(this.label13);
+            this.splitContainer1.Panel1.Controls.Add(this.label5);
+            this.splitContainer1.Panel1.Controls.Add(this.label12);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.label7);
+            this.splitContainer1.Panel1.Controls.Add(this.label11);
+            this.splitContainer1.Panel1.Controls.Add(this.label10);
+            this.splitContainer1.Panel1.Controls.Add(this.label9);
+            this.splitContainer1.Panel1.Controls.Add(this.label8);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox7);
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox6);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox5);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.label6);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox4);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox3);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.AxeLengthTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.AxeLengthLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.ParametersLabel);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.MaximumButton);
+            this.splitContainer1.Panel2.Controls.Add(this.AverageButton);
+            this.splitContainer1.Panel2.Controls.Add(this.MinimumButton);
+            this.splitContainer1.Panel2.Controls.Add(this.BuildButton);
+            this.splitContainer1.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel2_Paint);
+            this.splitContainer1.Size = new System.Drawing.Size(832, 482);
+            this.splitContainer1.SplitterDistance = 276;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label14.Location = new System.Drawing.Point(12, 444);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(92, 13);
+            this.label14.TabIndex = 15;
+            this.label14.Text = "Min - 19, Max - 25";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label13.Location = new System.Drawing.Point(12, 383);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(92, 13);
+            this.label13.TabIndex = 14;
+            this.label13.Text = "Min - 22, Max - 28";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 405);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Axe Part Second Width";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label12.Location = new System.Drawing.Point(12, 322);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(104, 13);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "Min - 135, Max - 165";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Axe Part First Width";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(11, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Axe Width";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label11.Location = new System.Drawing.Point(12, 261);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(104, 13);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Min - 135, Max - 165";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label10.Location = new System.Drawing.Point(12, 200);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(104, 13);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "Min - 170, Max - 215";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label9.Location = new System.Drawing.Point(12, 136);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(92, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Min - 48, Max - 68";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label8.Location = new System.Drawing.Point(12, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(104, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Min - 135, Max - 165";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(15, 113);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(258, 20);
+            this.textBox7.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Axe Part Height";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(15, 299);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(258, 20);
+            this.textBox6.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(15, 421);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(258, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Axe Height";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Axe Part Length";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 360);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(258, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 238);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(258, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(255, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // AxeLengthTextBox
+            // 
+            this.AxeLengthTextBox.BackColor = System.Drawing.Color.Red;
+            this.AxeLengthTextBox.Location = new System.Drawing.Point(15, 50);
+            this.AxeLengthTextBox.Name = "AxeLengthTextBox";
+            this.AxeLengthTextBox.Size = new System.Drawing.Size(258, 20);
+            this.AxeLengthTextBox.TabIndex = 2;
+            this.AxeLengthTextBox.TextChanged += new System.EventHandler(this.AxeLengthTextBox_TextChanged);
+            // 
+            // AxeLengthLabel
+            // 
+            this.AxeLengthLabel.AutoSize = true;
+            this.AxeLengthLabel.Location = new System.Drawing.Point(12, 34);
+            this.AxeLengthLabel.Name = "AxeLengthLabel";
+            this.AxeLengthLabel.Size = new System.Drawing.Size(61, 13);
+            this.AxeLengthLabel.TabIndex = 1;
+            this.AxeLengthLabel.Text = "Axe Length";
+            // 
+            // ParametersLabel
+            // 
+            this.ParametersLabel.AutoSize = true;
+            this.ParametersLabel.Location = new System.Drawing.Point(12, 9);
+            this.ParametersLabel.Name = "ParametersLabel";
+            this.ParametersLabel.Size = new System.Drawing.Size(60, 13);
+            this.ParametersLabel.TabIndex = 0;
+            this.ParametersLabel.Text = "Parameters";
+            this.ParametersLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MaximumButton
+            // 
+            this.MaximumButton.Location = new System.Drawing.Point(165, 447);
+            this.MaximumButton.Name = "MaximumButton";
+            this.MaximumButton.Size = new System.Drawing.Size(75, 23);
+            this.MaximumButton.TabIndex = 3;
+            this.MaximumButton.Text = "Maximum";
+            this.MaximumButton.UseVisualStyleBackColor = true;
+            this.MaximumButton.Click += new System.EventHandler(this.MaximumButton_Click);
+            // 
+            // AverageButton
+            // 
+            this.AverageButton.Location = new System.Drawing.Point(84, 447);
+            this.AverageButton.Name = "AverageButton";
+            this.AverageButton.Size = new System.Drawing.Size(75, 23);
+            this.AverageButton.TabIndex = 2;
+            this.AverageButton.Text = "Average";
+            this.AverageButton.UseVisualStyleBackColor = true;
+            this.AverageButton.Click += new System.EventHandler(this.AverageButton_Click);
+            // 
+            // MinimumButton
+            // 
+            this.MinimumButton.Location = new System.Drawing.Point(3, 447);
+            this.MinimumButton.Name = "MinimumButton";
+            this.MinimumButton.Size = new System.Drawing.Size(75, 23);
+            this.MinimumButton.TabIndex = 1;
+            this.MinimumButton.Text = "Minimum";
+            this.MinimumButton.UseVisualStyleBackColor = true;
+            this.MinimumButton.Click += new System.EventHandler(this.MinimumButton_Click);
+            // 
+            // BuildButton
+            // 
+            this.BuildButton.Location = new System.Drawing.Point(465, 447);
+            this.BuildButton.Name = "BuildButton";
+            this.BuildButton.Size = new System.Drawing.Size(75, 23);
+            this.BuildButton.TabIndex = 0;
+            this.BuildButton.Text = "Build";
+            this.BuildButton.UseVisualStyleBackColor = true;
+            this.BuildButton.Click += new System.EventHandler(this.BuildButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 482);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label ParametersLabel;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox AxeLengthTextBox;
+        private System.Windows.Forms.Label AxeLengthLabel;
+        private System.Windows.Forms.Button BuildButton;
+        private System.Windows.Forms.Button MaximumButton;
+        private System.Windows.Forms.Button AverageButton;
+        private System.Windows.Forms.Button MinimumButton;
+    }
+}
+
